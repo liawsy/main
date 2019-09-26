a6e7f92640afe73e36ac71dfe4c096b283777e35
liawsy <liawsy99@gmail.com>
2019-09-26 11:51:42 +0800

    Update test cases for Remark

--- src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -1,4 +1,32 @@
 package seedu.address.logic.parser;
 
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.model.person.Remark;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
 public class RemarkCommandParserTest {
+
+    private RemarkCommandParser parser = new RemarkCommandParser();
+    private final Remark remark = new Remark("some remark");
+
+    @Test
+    public void parse_specifiedIndex_success() {
+
+        //non empty remark
+        Index targetIndex = INDEX_FIRST_PERSON;
+        String userInput = targetIndex.getOneBased() + " " + PREFIX_REMARK + remark;
+        RemarkCommand command = new RemarkCommand(INDEX_FIRST_PERSON, remark);
+        assertParseSuccess(parser, userInput, command);
+
+        //empty remark
+        userInput = targetIndex.getOneBased() + " " + PREFIX_REMARK;
+        command = new RemarkCommand(INDEX_FIRST_PERSON, new Remark(""));
+        assertParseSuccess(parser, userInput, command);
+    }
+
 }
--- src/test/java/seedu/address/model/person/RemarkTest.java
+++ src/test/java/seedu/address/model/person/RemarkTest.java
@@ -1,4 +1,24 @@
 package seedu.address.model.person;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class RemarkTest {
+
+    @Test
+    public void equals() {
+        Remark testRemark = new Remark("test");
+        // same object -> returns true
+        assertTrue(testRemark.equals(testRemark));
+        // same values -> returns true
+        assertTrue(testRemark.equals(new Remark("test")));
+        // different types -> returns false
+        assertFalse(testRemark.equals(55));
+        // null -> returns false
+        assertFalse(testRemark.equals(null));
+        // different remarks -> returns false
+        assertFalse(testRemark.equals(new Remark("boo")));
+    }
 }
